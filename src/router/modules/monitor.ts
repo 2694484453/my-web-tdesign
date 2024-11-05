4eb49352b899a3d2ac1708601916ccfacc5caebf
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-11-06 02:30:46 +0800

    2024/11/5

--- src/router/modules/monitor.ts
+++ src/router/modules/monitor.ts
@@ -1,4 +1,4 @@
-import {HeartIcon, PreciseMonitorIcon} from 'tdesign-icons-vue';
+import {PreciseMonitorIcon,MirrorIcon} from 'tdesign-icons-vue';
 import Layout from '@/layouts/index.vue';
 
 export default [
@@ -41,6 +41,26 @@ export default [
         path: 'base',
         name: 'MonitorBase',
         component: () => import('@/pages/monitor-cloud/index.vue'),
+        meta: {title: '监控列表'},
+      }
+    ],
+  },
+  {
+    path: '/grafana',
+    name: 'grafana',
+    component: Layout,
+    meta: {title: 'grafana图表(主机版)', icon: MirrorIcon},
+    children: [
+      {
+        path: 'dashboard',
+        name: 'DashboardBase',
+        component: () => import('@/pages/grafana/dashboard.vue'),
+        meta: {title: '监控视图'},
+      },
+      {
+        path: 'base',
+        name: 'MonitorBase',
+        component: () => import('@/pages/grafana/index.vue'),
         meta: {title: '监控列表'},
       }
     ],
