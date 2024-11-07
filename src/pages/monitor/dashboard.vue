ce999c107849218c87e0dd9eff62bf5b8d8a7c1f
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-11-08 01:44:31 +0800

    2024/11/7

--- src/pages/monitor/dashboard.vue
+++ src/pages/monitor/dashboard.vue
@@ -19,7 +19,7 @@ export default {
     return {
       prefix,
       loading: true,
-      frameSrc: 'http://server.gpg123.vip:9090',
+      frameSrc: 'https://prometheus.server.gpg123.vip',
       settingStore: { ...STYLE_CONFIG },
       getWrapStyle: `height: ${window.innerHeight}px`,
     };
