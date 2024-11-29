8b0f6f371c340d623f42190f629c4627ebfd2440
gaopuguang_zz <gaopg09@chinatelecom.cn>
2024-11-30 02:40:08 +0800

    2024/11/26

--- src/router/modules/monitor.ts
+++ src/router/modules/monitor.ts
@@ -2,28 +2,27 @@ import {PreciseMonitorIcon, MirrorIcon, NotificationIcon} from 'tdesign-icons-vu
 import Layout from '@/layouts/index.vue';
 
 export default [
-  // 域名
-  {
-    path: '/prometheus',
-    name: 'prometheus',
-    component: Layout,
-    //redirect: '/prometheus/base',
-    meta: {title: '监控中心(主机版)', icon: PreciseMonitorIcon},
-    children: [
-      {
-        path: 'dashboard',
-        name: 'DashboardBase',
-        component: () => import('@/pages/prometheus/dashboard.vue'),
-        meta: {title: '监控视图'},
-      },
-      {
-        path: 'base',
-        name: 'MonitorBase',
-        component: () => import('@/pages/prometheus/index.vue'),
-        meta: {title: '监控列表'},
-      }
-    ],
-  },
+  // {
+  //   path: '/prometheus',
+  //   name: 'prometheus',
+  //   component: Layout,
+  //   //redirect: '/prometheus/base',
+  //   meta: {title: '监控中心(主机版)', icon: PreciseMonitorIcon},
+  //   children: [
+  //     {
+  //       path: 'dashboard',
+  //       name: 'DashboardBase',
+  //       component: () => import('@/pages/prometheus/dashboard.vue'),
+  //       meta: {title: '监控视图'},
+  //     },
+  //     {
+  //       path: 'base',
+  //       name: 'MonitorBase',
+  //       component: () => import('@/pages/prometheus/index.vue'),
+  //       meta: {title: '监控列表'},
+  //     }
+  //   ],
+  // },
   {
     path: '/prometheus-cloud',
     name: 'prometheus-cloud',
@@ -45,26 +44,26 @@ export default [
       }
     ],
   },
-  {
-    path: '/alert',
-    name: 'alert',
-    component: Layout,
-    meta: {title: '告警中心(主机版)', icon: NotificationIcon},
-    children: [
-      {
-        path: 'dashboard',
-        name: 'DashboardBase',
-        component: () => import('@/pages/alert/index.vue'),
-        meta: {title: '监控视图'},
-      },
-      {
-        path: "list",
-        name: 'listBase',
-        component: () => import("@/pages/alert/list/index.vue"),
-        meta: {title: '告警列表'},
-      }
-    ],
-  },
+  // {
+  //   path: '/alert',
+  //   name: 'alert',
+  //   component: Layout,
+  //   meta: {title: '告警中心(主机版)', icon: NotificationIcon},
+  //   children: [
+  //     {
+  //       path: 'dashboard',
+  //       name: 'DashboardBase',
+  //       component: () => import('@/pages/alert/index.vue'),
+  //       meta: {title: '监控视图'},
+  //     },
+  //     {
+  //       path: "list",
+  //       name: 'listBase',
+  //       component: () => import("@/pages/alert/list/index.vue"),
+  //       meta: {title: '告警列表'},
+  //     }
+  //   ],
+  // },
   {
     path: '/alert-cloud',
     name: 'alert-cloud',
--- src/router/modules/traefik.ts
+++ src/router/modules/traefik.ts
@@ -2,26 +2,26 @@ import {RectangleIcon} from 'tdesign-icons-vue';
 import Layout from '@/layouts/index.vue';
 
 export default [
-  {
-    path: '/traefik',
-    name: 'traefik',
-    component: Layout,
-    meta: {title: 'traefik管理(主机版)', icon: RectangleIcon},
-    children: [
-      {
-        path: 'dashboard',
-        name: 'DashboardBase',
-        component: () => import('@/pages/traefik/index-dashboard.vue'),
-        meta: {title: '服务面板'},
-      },
-      {
-        path: "list",
-        name: 'AddBase',
-        component: () => import("@/pages/traefik/index.vue"),
-        meta: {title: '配置列表'},
-      }
-    ],
-  },
+  // {
+  //   path: '/traefik',
+  //   name: 'traefik',
+  //   component: Layout,
+  //   meta: {title: 'traefik管理(主机版)', icon: RectangleIcon},
+  //   children: [
+  //     {
+  //       path: 'dashboard',
+  //       name: 'DashboardBase',
+  //       component: () => import('@/pages/traefik/index-dashboard.vue'),
+  //       meta: {title: '服务面板'},
+  //     },
+  //     {
+  //       path: "list",
+  //       name: 'AddBase',
+  //       component: () => import("@/pages/traefik/index.vue"),
+  //       meta: {title: '配置列表'},
+  //     }
+  //   ],
+  // },
   {
     path: '/traefik-cloud',
     name: 'traefik-cloud',
