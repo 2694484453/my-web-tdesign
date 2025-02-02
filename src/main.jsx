3cf50644f1c88cae2fb17d6cb3639b4e8af8c8f5
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-02-02 23:38:20 +0800

    2025/01/29

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,11 @@ import '@/style/index.less';
 
 import './permission';
 import store from './store';
+// op
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+
 
 Vue.use(VueRouter);
 Vue.use(TDesign);
@@ -33,6 +38,13 @@ VueRouter.prototype.replace = function replace(location) {
   return originReplace.call(this, location).catch((err) => err);
 };
 
+//optl
+const provider = new WebTracerProvider();
+const exporter = new OTLPTraceExporter({
+    url: 'http://124.221.2.29:4318/v1/traces', // 修改为你自己的collector地址
+});
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.register();
 Vue.config.productionTip = false;
 sync(store, router);
 new Vue({
