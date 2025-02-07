09e4b563b7c80339e7e87610ceadcc2667c52251
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-02-07 22:34:48 +0800

    2025/02/07

--- src/router/modules/domain.ts
+++ src/router/modules/domain.ts
@@ -3,19 +3,19 @@ import Layout from '@/layouts/index.vue';
 
 export default [
   // 域名
-  {
-    path: '/domain',
-    name: 'domain',
-    component: Layout,
-    redirect: '/domain/base',
-    meta: {title: '域名管理', icon: InternetIcon},
-    children: [
-      {
-        path: 'base',
-        name: 'DomainBase',
-        component: () => import('@/pages/domain/index.vue'),
-        meta: {title: '域名列表'},
-      },
-    ],
-  }
+  // {
+  //   path: '/domain',
+  //   name: 'domain',
+  //   component: Layout,
+  //   redirect: '/domain/base',
+  //   meta: {title: '域名管理', icon: InternetIcon},
+  //   children: [
+  //     {
+  //       path: 'base',
+  //       name: 'DomainBase',
+  //       component: () => import('@/pages/domain/index.vue'),
+  //       meta: {title: '域名列表'},
+  //     },
+  //   ],
+  // }
 ]
