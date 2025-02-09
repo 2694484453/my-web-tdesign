553c78fd52bd7751552418731262d92c56ddd79f
gaopuguang_zz <gaopg09@chinatelecom.cn>
2025-02-10 01:14:21 +0800

    2025/02/09

--- src/pages/ide/index.vue
+++ src/pages/ide/index.vue
@@ -1,86 +1,253 @@
 <template>
-  <div :class="`${prefix}-iframe-page`" :style="getWrapStyle">
-    <t-loading :loading="loading" size="large" :style="getWrapStyle">
-      <iframe ref="frameRef" :src="frameSrc" :class="`${prefix}-iframe-page__main`" @load="hideLoading"></iframe>
-    </t-loading>
+  <div class="dashboard-detail">
+    <t-card title="概览" class="dashboard-detail-card" :bordered="false">
+      <t-row :gutter="[16, 16]">
+        <t-col v-for="(item, index) in PANE_LIST_DATA" :key="index" :xs="6" :xl="3">
+          <t-card :class="['dashboard-list-card']" :description="item.title">
+            <div class="dashboard-list-card__number">{{ item.number }}</div>
+            <div class="dashboard-list-card__text">
+              <div class="dashboard-list-card__text-left">
+                环比
+                <trend class="icon" :type="item.upTrend ? 'up' : 'down'" :describe="item.upTrend || item.downTrend" />
+              </div>
+              <chevron-right-icon />
+            </div>
+          </t-card>
+        </t-col>
+        <t-col  :xs="6" :xl="3">
+          <t-card :class="['dashboard-list-card']" >
+            <div class="dashboard-list-card__number">11</div>
+            <div class="dashboard-list-card__text">
+              <div class="dashboard-list-card__text-left">
+                环比
+              </div>
+              <chevron-right-icon />
+            </div>
+          </t-card>
+        </t-col>
+      </t-row>
+    </t-card>
+    <t-row :gutter="[16, 16]" class="row-margin">
+      <t-col :xs="12" :xl="9">
+        <t-card :class="{ 'dashboard-detail-card': true }" title="采购商品申请趋势" subtitle="(件)" :bordered="false">
+          <template #actions>
+            <t-date-range-picker
+              style="width: 250px"
+              :default-value="LAST_7_DAYS"
+              theme="primary"
+              mode="date"
+              @change="onMaterialChange"
+            />
+          </template>
+          <div id="lineContainer" ref="lineContainer" style="width: 100%; height: 410px"></div>
+        </t-card>
+      </t-col>
+      <t-col :xs="12" :xl="3">
+        <product-card
+          v-for="(item, index) in PRODUCT_LIST"
+          :key="index"
+          :product="item"
+          :class="{ 'row-margin': index !== 0 }"
+        />
+      </t-col>
+    </t-row>
+    <t-card :class="{ 'dashboard-detail-card': true }" title="采购商品满意度分布" class="row-margin" :bordered="false">
+      <template #actions>
+        <t-date-range-picker
+          style="display: inline-block; margin-right: 8px; width: 250px"
+          :defaultValue="LAST_7_DAYS"
+          theme="primary"
+          mode="date"
+          @change="onSatisfyChange"
+        >
+        </t-date-range-picker>
+        <t-button>导出数据</t-button>
+      </template>
+      <div id="scatterContainer" style="width: 100%; height: 374px"></div>
+    </t-card>
   </div>
 </template>
 <script lang="ts">
-import { prefix } from '@/config/global';
-import STYLE_CONFIG from '@/config/style';
+import { GridComponent, TooltipComponent, LegendComponent } from 'echarts/components';
+import { LineChart, ScatterChart } from 'echarts/charts';
+import { CanvasRenderer } from 'echarts/renderers';
+import * as echarts from 'echarts/core';
+import { mapState } from 'vuex';
+import { ChevronRightIcon } from 'tdesign-icons-vue';
 
-const computedStyle = getComputedStyle(document.documentElement);
-const sizeXxxl = computedStyle.getPropertyValue('--td-comp-size-xxxl');
-const paddingTBXxl = computedStyle.getPropertyValue('--td-comp-paddingTB-xxl');
+import Trend from '@/components/trend/index.vue';
+import ProductCard from '@/components/product-card/index.vue';
+
+import { LAST_7_DAYS } from '@/utils/date';
+import { changeChartsTheme } from '@/utils/color';
+
+import { PANE_LIST_DATA, PRODUCT_LIST } from '@/service/service-detail';
+import { getFolderLineDataSet, getScatterDataSet } from './index';
+
+echarts.use([GridComponent, LegendComponent, TooltipComponent, LineChart, ScatterChart, CanvasRenderer]);
 
 export default {
-  name: 'DetailAdvanced',
+  name: 'DashboardDetail',
+  components: { Trend, ProductCard, ChevronRightIcon },
   data() {
     return {
-      prefix,
-      loading: true,
-      frameSrc: 'https://alertmanager.server.gpg123.vip',
-      settingStore: { ...STYLE_CONFIG },
-      getWrapStyle: `height: ${window.innerHeight}px`,
+      PANE_LIST_DATA,
+      PRODUCT_LIST,
+      dashboardBase: '',
+      lineContainer: '',
+      scatterContainer: '',
+      lineChart: '',
+      scatterChart: '',
+      productList: [
+        {
+          description: 'SSL证书又叫服务器证书，腾讯云为您提供证书的一站式服务，包括免费、付费证书的申请、管理及部',
+          index: 1,
+          isSetup: true,
+          name: 'SSL证书',
+          type: 4,
+        },
+      ],
+      LAST_7_DAYS,
     };
   },
+  computed: {
+    ...mapState('setting', ['brandTheme', 'mode']),
+  },
+  watch: {
+    brandTheme() {
+      changeChartsTheme([this.lineChart, this.scatterChart]);
+    },
+    mode() {
+      this.renderCharts();
+    },
+  },
   mounted() {
-    window.addEventListener('resize', this.calcHeight, false);
     this.$nextTick(() => {
-      this.calcHeight();
+      this.updateContainer();
     });
+    this.renderCharts();
   },
   methods: {
-    hideLoading() {
-      this.loading = false;
-      this.calcHeight();
-    },
+    /** 采购商品满意度选择 */
+    onSatisfyChange(value: string) {
+      const { chartColors } = this.$store.state.setting;
 
-    getOuterHeight(dom: Element) {
-      let height = dom.clientHeight;
-      const computedStyle = window.getComputedStyle(dom);
-      height += parseInt(computedStyle.marginTop, 10);
-      height += parseInt(computedStyle.marginBottom, 10);
-      height += parseInt(computedStyle.borderTopWidth, 10);
-      height += parseInt(computedStyle.borderBottomWidth, 10);
-      return height;
+      this.scatterChart.setOption(getScatterDataSet({ dateTime: value, ...chartColors }));
     },
+    /** 采购商品申请趋势选择 */
+    onMaterialChange(value: string) {
+      const { chartColors } = this.$store.state.setting;
 
-    calcHeight() {
-      const iframe = this.$refs.frameRef;
-      if (!iframe) {
-        return;
+      this.lineChart.setOption(getFolderLineDataSet({ dateTime: value, ...chartColors }));
+    },
+    updateContainer() {
+      this.lineChart.resize?.({
+        width: this.lineContainer.clientWidth,
+        height: this.lineContainer.clientHeight,
+      });
+      this.scatterChart.resize?.({
+        width: this.scatterContainer.clientWidth,
+        height: this.scatterContainer.clientHeight,
+      });
+    },
+    renderCharts() {
+      const { chartColors } = this.$store.state.setting;
+
+      if (!this.lineContainer) {
+        this.lineContainer = document.getElementById('lineContainer');
       }
-      let clientHeight = 0;
-      const { showFooter, isUseTabsRouter, showBreadcrumb } = this.settingStore;
-      const headerHeight = parseFloat(sizeXxxl);
-      const navDom = document.querySelector('.t-tabs__nav');
-      const navHeight = isUseTabsRouter ? this.getOuterHeight(navDom) : 0;
-      const breadcrumbDom = document.querySelector('.t-breadcrumb');
-      const breadcrumbHeight = showBreadcrumb ? this.getOuterHeight(breadcrumbDom) : 0;
-      const contentPadding = parseFloat(paddingTBXxl) * 2;
-      const footerDom = document.querySelector('.t-layout__footer');
-      const footerHeight = showFooter ? this.getOuterHeight(footerDom) : 0;
-      const top = headerHeight + navHeight + breadcrumbHeight + contentPadding + footerHeight + 2;
-      window.innerHeight -= top;
-      clientHeight = document.documentElement.clientHeight - top;
-      iframe.style.height = `${clientHeight}px`;
-      this.getWrapStyle = `height: ${window.innerHeight}px`;
+      this.lineChart = echarts.init(this.lineContainer);
+      this.lineChart.setOption(getFolderLineDataSet({ ...chartColors }));
+
+      window.addEventListener('resize', this.updateContainer, false);
+
+      if (!this.scatterContainer) {
+        this.scatterContainer = document.getElementById('scatterContainer');
+      }
+      this.scatterChart = echarts.init(this.scatterContainer);
+      this.scatterChart.setOption(getScatterDataSet({ ...chartColors }));
     },
   },
 };
 </script>
 <style lang="less" scoped>
-@import '@/style/variables';
+@import '@/style/variables.less';
 
-.@{starter-prefix}-iframe-page {
-  &__main {
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    background-color: #fff;
-    border: 0;
-    box-sizing: border-box;
+.row-margin {
+  margin-top: 16px;
+}
+
+// 统一增加8px;
+.dashboard-detail-card {
+  padding: 8px;
+
+  /deep/ .t-card__title {
+    font-size: 20px;
+    font-weight: 500;
+  }
+
+  /deep/ .t-card__actions {
+    display: flex;
+    align-items: center;
+  }
+}
+
+.dashboard-list-card {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  height: 170px;
+  padding: 8px;
+
+  /deep/ .t-card__header {
+    padding-bottom: 8px;
+  }
+
+  /deep/ .t-card__body {
+    flex: 1;
+    display: flex;
+    padding-top: 0;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  &.dark {
+    &:hover {
+      background: var(--td-gray-color-14);
+      cursor: pointer;
+    }
+  }
+
+  &.light {
+    &:hover {
+      background: var(--td-gray-color-14);
+      cursor: pointer;
+    }
+  }
+
+  &__number {
+    font-size: 36px;
+    line-height: 44px;
+    color: var(--td-text-color-primary);
+  }
+
+  &__text {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    color: var(--td-text-color-placeholder);
+    text-align: left;
+    line-height: 18px;
+
+    &-left {
+      display: flex;
+
+      .icon {
+        margin: 0 8px;
+      }
+    }
   }
 }
 </style>
--- src/pages/ide/list/index.vue
+++ src/pages/ide/list/index.vue
@@ -1,318 +1,86 @@
 <template>
-  <div>
-    <t-card class="list-card-container" :bordered="false">
-      <t-form
-        ref="form"
-        :data="formData"
-        :label-width="80"
-        colon
-        @reset="onReset"
-        @submit="onSubmit"
-        :style="{ marginBottom: '8px' }"
-      >
-        <t-row justify="space-between">
-          <div class="left-operation-container">
-            <t-button @click="handleSetupContract"> 新建</t-button>
-            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出</t-button>
-            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>
-          </div>
-          <t-input v-model="searchValue" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
-              <template #suffix-icon>
-                <search-icon size="20px"/>
-                </template>
-          </t-input>
-          <t-col :span="2" class="operation-container">
-            <t-button theme="primary" type="submit" :style="{ marginLeft: '8px' }"> 查询</t-button>
-            <t-button type="reset" variant="base" theme="default"> 重置</t-button>
-          </t-col>
-        </t-row>
-      </t-form>
-      <div class="table-container">
-        <t-table
-          :columns="columns"
-          :data="data"
-          :rowKey="rowKey"
-          :verticalAlign="verticalAlign"
-          :hover="hover"
-          :pagination="pagination"
-          :selected-row-keys="selectedRowKeys"
-          :loading="dataLoading"
-          @page-change="rehandlePageChange"
-          @change="rehandleChange"
-          @select-change="rehandleSelectChange"
-          :headerAffixedTop="true"
-          :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"
-        >
-          <template #status="{ row }">
-            <t-tag v-if="row.status === CONTRACT_STATUS.FAIL" theme="danger" variant="light">审核失败</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.AUDIT_PENDING" theme="warning" variant="light">待审核</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXEC_PENDING" theme="warning" variant="light">待履行</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXECUTING" theme="success" variant="light">履行中</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.FINISH" theme="success" variant="light">已完成</t-tag>
-          </template>
-          <template #contractType="{ row }">
-            <p v-if="row.contractType === CONTRACT_TYPES.MAIN">审核失败</p>
-            <p v-if="row.contractType === CONTRACT_TYPES.SUB">待审核</p>
-            <p v-if="row.contractType === CONTRACT_TYPES.SUPPLEMENT">待履行</p>
-          </template>
-          <template #paymentType="{ row }">
-            <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.PAYMENT" class="payment-col">
-              付款
-              <trend class="dashboard-item-trend" type="up"/>
-            </p>
-            <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.RECEIPT" class="payment-col">
-              收款
-              <trend class="dashboard-item-trend" type="down"/>
-            </p>
-          </template>
-          <template #op="slotProps">
-            <a class="t-button-link" @click="handleClickDetail()">详情</a>
-            <a class="t-button-link" @click="handleClickDelete(slotProps)">删除</a>
-          </template>
-        </t-table>
-      </div>
-    </t-card>
-    <t-dialog
-      header="确认删除当前所选合同？"
-      :body="confirmBody"
-      :visible.sync="confirmVisible"
-      @confirm="onConfirmDelete"
-      :onCancel="onCancel"
-    >
-    </t-dialog>
+  <div :class="`${prefix}-iframe-page`" :style="getWrapStyle">
+    <t-loading :loading="loading" size="large" :style="getWrapStyle">
+      <iframe ref="frameRef" :src="frameSrc" :class="`${prefix}-iframe-page__main`" @load="hideLoading"></iframe>
+    </t-loading>
   </div>
 </template>
 <script lang="ts">
-import Vue from 'vue';
-import {SearchIcon} from 'tdesign-icons-vue';
-import Trend from '@/components/trend/index.vue';
-import {prefix} from '@/config/global';
+import { prefix } from '@/config/global';
+import STYLE_CONFIG from '@/config/style';
 
-import {CONTRACT_STATUS, CONTRACT_STATUS_OPTIONS, CONTRACT_TYPES, CONTRACT_PAYMENT_TYPES} from '@/constants';
+const computedStyle = getComputedStyle(document.documentElement);
+const sizeXxxl = computedStyle.getPropertyValue('--td-comp-size-xxxl');
+const paddingTBXxl = computedStyle.getPropertyValue('--td-comp-paddingTB-xxl');
 
-export default Vue.extend({
-  name: 'ListBase',
-  components: {
-    SearchIcon,
-    Trend,
-  },
+export default {
+  name: 'DetailAdvanced',
   data() {
     return {
-      CONTRACT_STATUS,
-      CONTRACT_STATUS_OPTIONS,
-      CONTRACT_TYPES,
-      CONTRACT_PAYMENT_TYPES,
       prefix,
-      dataLoading: false,
-      data: [],
-      selectedRowKeys: [1, 2],
-      value: 'first',
-      columns: [
-        {
-          title: '名称',
-          align: 'left',
-          width: 200,
-          ellipsis: true,
-          colKey: 'name',
-          fixed: 'left',
-        },
-        {
-          title: '类型',
-          width: 200,
-          ellipsis: true,
-          fixed: 'left',
-          colKey: 'type',
-        },
-        {
-          title: '状态',
-          colKey: 'status',
-          width: 100, cell:
-            {col: 'status'}
-        },
-        {
-          title: '标签',
-          width: 200,
-          ellipsis: true,
-          colKey: 'labels',
-        },
-        {
-          title: '端点',
-          width: 200,
-          ellipsis: true,
-          colKey: "targets"
-        },
-        {
-          align: 'left',
-          fixed: 'right',
-          width: 150,
-          colKey: 'op',
-          title: '操作',
-        },
-      ],
-      rowKey: 'index',
-      tableLayout: 'auto',
-      verticalAlign: 'top',
-      hover: true,
-      rowClassName: (rowKey: string) => `${rowKey}-class`,
-      // 与pagination对齐
-      pagination: {
-        defaultPageSize: 10,
-        total: 0,
-        defaultCurrent: 1,
-      },
-      searchValue: '',
-      confirmVisible: false,
-      deleteIdx: -1,
-      deleteType: -1,
-      formData: {
-        name: "",
-        type: ""
-      },
-      typeList: []
+      loading: true,
+      frameSrc: 'https://code-server.gpg123.vip',
+      settingStore: { ...STYLE_CONFIG },
+      getWrapStyle: `height: ${window.innerHeight}px`,
     };
   },
-  computed: {
-    confirmBody() {
-      if (this.deleteIdx > -1) {
-        const {name} = this.data?.[this.deleteIdx];
-        return `删除后，${name}的所有合同信息将被清空，且无法恢复`;
-      }
-      return '';
-    },
-    offsetTop() {
-      return this.$store.state.setting.isUseTabsRouter ? 48 : 0;
-    },
-  },
   mounted() {
-  },
-  created() {
-    this.getTypeList()
-    this.getList()
+    window.addEventListener('resize', this.calcHeight, false);
+    this.$nextTick(() => {
+      this.calcHeight();
+    });
   },
   methods: {
-    getContainer() {
-      return document.querySelector('.tdesign-starter-layout');
+    hideLoading() {
+      this.loading = false;
+      this.calcHeight();
     },
-    rehandlePageChange(curr, pageInfo) {
-      console.log('分页变化', curr, pageInfo);
+
+    getOuterHeight(dom: Element) {
+      let height = dom.clientHeight;
+      const computedStyle = window.getComputedStyle(dom);
+      height += parseInt(computedStyle.marginTop, 10);
+      height += parseInt(computedStyle.marginBottom, 10);
+      height += parseInt(computedStyle.borderTopWidth, 10);
+      height += parseInt(computedStyle.borderBottomWidth, 10);
+      return height;
     },
-    rehandleSelectChange(selectedRowKeys: number[]) {
-      this.selectedRowKeys = selectedRowKeys;
-    },
-    rehandleChange(changeParams, triggerAndData) {
-      console.log('统一Change', changeParams, triggerAndData);
-    },
-    handleClickDetail() {
-      this.$router.push('/detail/base');
-    },
-    handleSetupContract() {
-      this.$router.push('/prometheus/add');
-    },
-    handleClickDelete(row: { rowIndex: any,type: any }) {
-      this.deleteIdx = row.rowIndex;
-      this.deleteType = row.type;
-      this.confirmVisible = true;
-      console.log("this",this.deleteType)
-    },
-    onConfirmDelete() {
-      // 真实业务请发起请求
-      this.data.splice(this.deleteIdx, 1);
-      this.pagination.total = this.data.length;
-      const selectedIdx = this.selectedRowKeys.indexOf(this.deleteIdx);
-      if (selectedIdx > -1) {
-        this.selectedRowKeys.splice(selectedIdx, 1);
+
+    calcHeight() {
+      const iframe = this.$refs.frameRef;
+      if (!iframe) {
+        return;
       }
-      this.confirmVisible = false;
-      // 请求删除
-      this.$request.delete("/monitor/delete",{
-        params: {
-          index: this.deleteIdx,
-          type: this.deleteType
-        }
-      }).then(res=>{
-        this.$message.success(res.data.msg);
-      }).catch(err=>{
-
-      })
-
-      this.resetIdx();
+      let clientHeight = 0;
+      const { showFooter, isUseTabsRouter, showBreadcrumb } = this.settingStore;
+      const headerHeight = parseFloat(sizeXxxl);
+      const navDom = document.querySelector('.t-tabs__nav');
+      const navHeight = isUseTabsRouter ? this.getOuterHeight(navDom) : 0;
+      const breadcrumbDom = document.querySelector('.t-breadcrumb');
+      const breadcrumbHeight = showBreadcrumb ? this.getOuterHeight(breadcrumbDom) : 0;
+      const contentPadding = parseFloat(paddingTBXxl) * 2;
+      const footerDom = document.querySelector('.t-layout__footer');
+      const footerHeight = showFooter ? this.getOuterHeight(footerDom) : 0;
+      const top = headerHeight + navHeight + breadcrumbHeight + contentPadding + footerHeight + 2;
+      window.innerHeight -= top;
+      clientHeight = document.documentElement.clientHeight - top;
+      iframe.style.height = `${clientHeight}px`;
+      this.getWrapStyle = `height: ${window.innerHeight}px`;
     },
-    onCancel() {
-      this.resetIdx();
-    },
-    resetIdx() {
-      this.deleteIdx = -1;
-    },
-    onReset(data) {
-      console.log(data);
-    },
-    onSubmit(data) {
-      console.log(this.formData);
-      this.getList(this.formData);
-    },
-    getTypeList() {
-      this.$request.get("/monitor/typeList").then(res => {
-        this.typeList = res.data.data
-      }).catch((err) => {
-
-      })
-    },
-    getList() {
-      this.dataLoading = true;
-      this.$request
-        .get('/ide/page',{
-          params: this.formData
-        }).then((res) => {
-        if (res.data.code === 200) {
-          //console.log(res.data.data)
-          this.data = res.data.data;
-          //console.log(this.data)
-          this.pagination = {
-            ...this.pagination,
-            total: res.data.data.length,
-          };
-        }
-      })
-        .catch((e: Error) => {
-          console.log(e);
-        })
-        .finally(() => {
-          this.dataLoading = false;
-        });
-    }
   },
-});
+};
 </script>
-
 <style lang="less" scoped>
-@import '@/style/variables';
+@import 'variables';
 
-.payment-col {
-  display: flex;
-
-  .trend-container {
-    display: flex;
-    align-items: center;
-    margin-left: 8px;
+.@{starter-prefix}-iframe-page {
+  &__main {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    background-color: #fff;
+    border: 0;
+    box-sizing: border-box;
   }
-}
-
-.left-operation-container {
-  padding: 0 0 6px 0;
-  margin-bottom: 16px;
-
-  .selected-count {
-    display: inline-block;
-    margin-left: var(--td-comp-margin-s);
-    color: var(--td-text-color-secondary);
-  }
-}
-
-.search-input {
-  width: 360px;
-}
-
-.t-button + .t-button {
-  margin-left: var(--td-comp-margin-s);
 }
 </style>
