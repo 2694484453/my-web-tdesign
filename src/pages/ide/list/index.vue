013bab934be4a711bf25f79f89d7c4f0109b5389
‘gaopuguang_zz’ <gaopg09@chinatelecom.cn>
2025-02-14 15:42:55 +0800

    2025/2/14

--- src/pages/ide/list/index.vue
+++ src/pages/ide/list/index.vue
@@ -12,9 +12,9 @@
       >
         <t-row justify="space-between">
           <div class="left-operation-container">
-            <t-button @click="handleSetupContract"> 新建</t-button>
-            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出</t-button>
-            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>
+<!--            <t-button @click="handleSetupContract"> 新建</t-button>-->
+<!--            <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出</t-button>-->
+<!--            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
           </div>
           <t-input v-model="searchValue" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
               <template #suffix-icon>
