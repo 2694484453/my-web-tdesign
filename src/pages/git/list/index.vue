0512e3656344b091a6968a885988c7b830a6c3db
gaopuguang <2694484453@qq.com>
2025-05-09 19:44:59 +0800

    2025/05/08 调整

--- src/pages/git/list/index.vue
+++ src/pages/git/list/index.vue
@@ -14,7 +14,7 @@
           <div class="left-operation-container">
             <t-button @click="handleSetupContract">导入</t-button>
             <t-button variant="base" theme="default" :disabled="!selectedRowKeys.length"> 导出</t-button>
-            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>
+            <!--            <p v-if="!!selectedRowKeys.length" class="selected-count">已选{{ selectedRowKeys.length }}项</p>-->
           </div>
           <t-input v-model="searchValue" class="search-input" placeholder="请输入你需要搜索的内容" clearable>
             <template #suffix-icon>
@@ -40,33 +40,18 @@
           :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"
         >
           <template #status="{ row }">
-            <t-tag v-if="row.status === CONTRACT_STATUS.FAIL" theme="danger" variant="light">校验失败</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.AUDIT_PENDING" theme="warning" variant="light">打包失败</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXEC_PENDING" theme="warning" variant="light">未知</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.EXECUTING" theme="success" variant="light">打包中</t-tag>
-            <t-tag v-if="row.status === CONTRACT_STATUS.FINISH" theme="success" variant="light">已完成</t-tag>
-          </template>
-          <template #contractType="{ row }">
-            <p v-if="row.contractType === CONTRACT_TYPES.MAIN">审核失败</p>
-            <p v-if="row.contractType === CONTRACT_TYPES.SUB">待审核</p>
-            <p v-if="row.contractType === CONTRACT_TYPES.SUPPLEMENT">待履行</p>
-          </template>
-          <template #paymentType="{ row }">
-            <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.PAYMENT" class="payment-col">
-              推送成功
-              <trend class="dashboard-item-trend" type="up"/>
-            </p>
-            <p v-if="row.paymentType === CONTRACT_PAYMENT_TYPES.RECEIPT" class="payment-col">
-              推送失败
-              <trend class="dashboard-item-trend" type="down"/>
-            </p>
+            <!--            <t-tag v-if="row.status === CONTRACT_STATUS.FAIL" theme="danger" variant="light">校验失败</t-tag>-->
+            <!--            <t-tag v-if="row.status === CONTRACT_STATUS.AUDIT_PENDING" theme="warning" variant="light">打包失败</t-tag>-->
+            <!--            <t-tag v-if="row.status === CONTRACT_STATUS.EXEC_PENDING" theme="warning" variant="light">未知</t-tag>-->
+            <!--            <t-tag v-if="row.status === CONTRACT_STATUS.EXECUTING" theme="success" variant="light">打包中</t-tag>-->
+            <!--            <t-tag v-if="row.status === CONTRACT_STATUS.FINISH" theme="success" variant="light">已完成</t-tag>-->
           </template>
           <template #language="{ row }">
-            <p v-if="row.language === '' || row.language === null ">未知</p>
-            <p v-else>{{row.language}}</p>
+            <p v-if="row.language === '' || row.language === null">未知</p>
+            <p v-else>{{ row.language }}</p>
           </template>
           <template #html_url="{ row }">
-            <a :href="row.html_url" target="_blank">{{row.html_url}}</a>
+            <a :href="row.html_url" target="_blank">{{ row.html_url }}</a>
           </template>
           <template #op="slotProps">
             <a class="t-button-link" @click="handleClickSuccess()">在线预览</a>
@@ -92,6 +77,176 @@
       :onCancel="onCancel"
     >
     </t-dialog>
+    <t-space>
+      <t-dialog
+        v-model:visible="dialog.visible"
+        :header="dialog.header"
+        width="40%"
+        :confirm-on-enter="true"
+        :on-cancel="onCancel"
+        :on-close-btn-click="onCloseDialogBtnClick"
+        :on-confirm="onConfirmDialog"
+      >
+        <t-space direction="vertical" style="width: 100%">
+          <div>
+            <t-card :bordered="false">
+              <t-form
+                ref="form"
+                :data="formData"
+                :rules="FORM_RULES"
+                label-align="top"
+                :label-width="100"
+                @reset="onReset"
+                @submit="onSubmit"
+              >
+                <div class="form-basic-container">
+                  <div class="form-basic-item">
+                    <div class="form-basic-container-title">合同信息</div>
+                    <!-- 表单内容 -->
+                    <!-- 合同名称,合同类型 -->
+                    <t-row class="row-gap" :gutter="[16, 24]">
+                      <t-col :span="6">
+                        <t-form-item label="合同名称" name="name">
+                          <t-input v-model="formData.name" :style="{ width: '322px' }" placeholder="请输入内容"/>
+                        </t-form-item>
+                      </t-col>
+                      <t-col :span="6">
+                        <t-form-item label="合同类型" name="type">
+                          <t-select
+                            v-model="formData.type"
+                            :style="{ width: '322px' }"
+                            placeholder="请选择类型"
+                            class="demo-select-base"
+                            clearable
+                          >
+                            <t-option v-for="(item, index) in typeOptions" :key="index" :value="item.value"
+                                      :label="item.label">
+                              {{ item.label }}
+                            </t-option>
+                          </t-select>
+                        </t-form-item>
+                      </t-col>
+                      <!-- 合同收付类型 -->
+                      <t-col :span="8">
+                        <t-form-item label="合同收付类型" name="payment">
+                          <t-radio-group v-model="formData.payment">
+                            <t-radio value="1"> 收款</t-radio>
+                            <t-radio value="2"> 付款</t-radio>
+                          </t-radio-group>
+                          <span class="space-item"/>
+                          <t-input placeholder="请输入金额" :style="{ width: '160px' }"/>
+                        </t-form-item>
+                      </t-col>
+
+                      <t-col :span="6">
+                        <t-form-item label="甲方" name="partyA">
+                          <t-select
+                            v-model="formData.partyA"
+                            :style="{ width: '322px' }"
+                            class="demo-select-base"
+                            placeholder="请选择类型"
+                            clearable>
+                            <t-option v-for="(item, index) in partyAOptions" :key="index" :value="item.value"
+                                      :label="item.label">
+                              {{ item.label }}
+                            </t-option>
+                          </t-select>
+                        </t-form-item>
+                      </t-col>
+                      <t-col :span="6">
+                        <t-form-item label="乙方" name="partyB">
+                          <t-select
+                            v-model="formData.partyB"
+                            :style="{ width: '322px' }"
+                            placeholder="请选择类型"
+                            class="demo-select-base"
+                            clearable
+                          >
+                            <t-option v-for="(item, index) in partyBOptions" :key="index" :value="item.value"
+                                      :label="item.label">
+                              {{ item.label }}
+                            </t-option>
+                          </t-select>
+                        </t-form-item>
+                      </t-col>
+                      <t-col :span="6">
+                        <t-form-item label="合同签订日期" name="signDate">
+                          <t-date-picker
+                            v-model="formData.signDate"
+                            :style="{ width: '322px' }"
+                            theme="primary"
+                            mode="date"
+                            separator="/"
+                          />
+                        </t-form-item>
+                      </t-col>
+                      <t-col :span="6">
+                        <t-form-item label="合同生效日期" name="startDate">
+                          <t-date-picker
+                            v-model="formData.startDate"
+                            :style="{ width: '322px' }"
+                            theme="primary"
+                            mode="date"
+                            separator="/"
+                          />
+                        </t-form-item>
+                      </t-col>
+                      <t-col :span="6">
+                        <t-form-item label="合同结束日期" name="endDate">
+                          <t-date-picker
+                            v-model="formData.endDate"
+                            :style="{ width: '322px' }"
+                            theme="primary"
+                            mode="date"
+                            separator="/"
+                          />
+                        </t-form-item>
+                      </t-col>
+                      <t-col :span="6">
+                        <t-form-item label="" name="files">
+                          <t-upload
+                            v-model="formData.files"
+                            action="https://service-bv448zsw-1257786608.gz.apigw.tencentcs.com/api/upload-demo"
+                            tips="请上传pdf文件，大小在60M以内"
+                            :size-limit="{ size: 60, unit: 'MB' }"
+                            :format-response="formatResponse"
+                            :before-upload="beforeUpload"
+                            @fail="handleFail"
+                          >
+                            <t-button class="form-submit-upload-btn" variant="outline"> 上传合同文件</t-button>
+                          </t-upload>
+                        </t-form-item>
+                      </t-col>
+                    </t-row>
+                    <div class="form-basic-container-title form-title-gap">其它信息</div>
+                    <t-form-item label="备注" name="comment">
+                      <t-textarea v-model="formData.comment" :height="124" placeholder="请输入备注"/>
+                    </t-form-item>
+                    <t-form-item label="公证人">
+                      <t-avatar-group>
+                        <t-avatar>D</t-avatar>
+                        <t-avatar>S</t-avatar>
+                        <t-avatar>+</t-avatar>
+                      </t-avatar-group>
+                    </t-form-item>
+                  </div>
+                </div>
+
+                <div class="form-submit-container">
+                  <div class="form-submit-sub">
+                    <div class="form-submit-left">
+                      <t-button theme="primary" class="form-submit-confirm" type="submit"> 提交</t-button>
+                      <t-button type="reset" class="form-submit-cancel" theme="default" variant="base"> 取消</t-button>
+                    </div>
+                  </div>
+                </div>
+              </t-form>
+            </t-card>
+          </div>
+          <t-pagination v-model="dialog.current" v-model:pageSize="dialog.pageSize" :total="30"/>
+        </t-space>
+      </t-dialog>
+    </t-space>
   </div>
 </template>
 <script lang="ts">
@@ -144,19 +299,19 @@ export default Vue.extend({
           title: '地址',
           width: 200,
           ellipsis: true,
-          colKey: 'html_url',
+          colKey: 'gitUrl',
         },
         {
           title: '创建时间',
           width: 200,
           ellipsis: true,
-          colKey: 'created_at',
+          colKey: 'createTime',
         },
         {
           title: '更新时间',
           width: 200,
           ellipsis: true,
-          colKey: 'updated_at',
+          colKey: 'updateTime',
         },
         {
           title: '描述',
@@ -192,6 +347,12 @@ export default Vue.extend({
         pageNum: 1,
         pageSize: 10
       },
+      dialog: {
+        visible: false,
+        header: "仓库导入",
+        current: 1,
+        pageSize: 10,
+      }
     };
   },
   computed: {
@@ -214,17 +375,17 @@ export default Vue.extend({
   methods: {
     getList() {
       this.dataLoading = true;
-      this.$request.get('/gitRepo/page',{
-          params: this.formData
-        }).then((res) => {
-          if (res.data.code === 200) {
-            this.data = res.data.rows;
-            this.pagination = {
-              ...this.pagination,
-              total: res.data.total,
-            };
-          }
-        })
+      this.$request.get('/git/repo/page', {
+        params: this.formData
+      }).then((res) => {
+        if (res.data.code === 200) {
+          this.data = res.data.rows;
+          this.pagination = {
+            ...this.pagination,
+            total: res.data.total,
+          };
+        }
+      })
         .catch((e: Error) => {
           console.log(e);
         })
@@ -255,8 +416,7 @@ export default Vue.extend({
       this.$emit('transfer', "detail", row)
     },
     handleSetupContract() {
-      //this.$router.push('/build/helmForm');
-      this.$emit('transfer', "form")
+      this.dialog.visible = true
     },
     handleClickDelete(row: { rowIndex: any }) {
       this.deleteIdx = row.rowIndex;
@@ -280,6 +440,14 @@ export default Vue.extend({
       this.confirmVisible = false;
       this.$message.success('删除成功');
       this.resetIdx();
+    },
+    // 确认对话框
+    onConfirmDialog() {
+
+    },
+    // 确定关闭对话框
+    onCloseDialogBtnClick() {
+
     },
     onCancel() {
       this.resetIdx();
