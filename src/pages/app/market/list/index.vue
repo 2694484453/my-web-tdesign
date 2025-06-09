10120050272a161d4294b33fe7d251e73fda608b
gaopuguang <2694484453@qq.com>
2025-06-10 01:54:58 +0800

    2025/06/03 调整

--- src/pages/app/market/list/index.vue
+++ src/pages/app/market/list/index.vue
@@ -35,18 +35,20 @@
           :headerAffixProps="{ offsetTop: offsetTop, container: getContainer }"
         >
           <template #repoUrl="{ row }">
-            <a v-bind:href="row.repoUrl" target="_blank">{{row.repoUrl}}</a>
+            <a v-bind:href="row.repoUrl" target="_blank">{{ row.repoUrl }}</a>
           </template>
           <template #generated="{ row }">
-            <p>{{new Date(row.generated).toLocaleString()}}</p>
+            <p>{{ new Date(row.generated).toLocaleString() }}</p>
           </template>
-          <template #isInstalled="{ row }">
-            <t-tag :theme="row.isInstalled ? 'success':'default'" variant="light">{{row.isInstalled ? "已安装" : "未安装"}}</t-tag>
+          <template #status="{ row }">
+            <t-tag v-show="row.status === 'success'" theme="success" variant="light">更新成功</t-tag>
+            <t-tag v-show="row.status === 'fail'" theme="danger" variant="light">更新失败</t-tag>
           </template>
           <template #op="slotProps">
-            <a v-show="!slotProps.row.isInstalled" class="t-button-link" @click="form.visible=true;handleClickInstall(slotProps.row)">安装</a>
+            <a v-show="!slotProps.row.isInstalled" class="t-button-link" @click="handleClickUpdate(slotProps.row)">更新</a>
             <a class="t-button-link" @click="form.visible=true;handleClickDetail()">详情</a>
-            <a v-show="slotProps.row.isInstalled" class="t-button-link" @click="handleClickDelete(slotProps)">卸载</a>
+            <a class="t-button-link" @click="form.visible=true;handleClickDetail()">修改</a>
+            <a v-show="slotProps.row.isInstalled" class="t-button-link" @click="handleClickDelete(slotProps)">删除</a>
           </template>
         </t-table>
         <div style="margin-top: 10px">
@@ -62,10 +64,10 @@
       </div>
     </t-card>
     <t-dialog
-      header="确认删除当前所选？"
-      :body="confirmBody"
-      :visible.sync="confirmVisible"
-      @confirm="onConfirmDelete"
+      :header="confirm.header"
+      :body="confirm.body"
+      :visible.sync="confirm.visible"
+      @confirm="handleClickConfirm"
       :onCancel="onCancel">
     </t-dialog>
     <!--抽屉-->
@@ -120,39 +122,33 @@ export default Vue.extend({
       value: 'first',
       columns: [
         {
-          title: '名称',
+          title: '仓库名称',
           align: 'left',
-          width: 180,
+          width: 120,
           ellipsis: true,
-          colKey: 'name',
+          colKey: 'repoName',
           fixed: 'left',
+        },
+        {
+          title: '制品数量',
+          width: 80,
+          ellipsis: true,
+          fixed: 'left',
+          colKey: 'artifactTotal',
         },
         {
           title: '版本数量',
           width: 80,
           ellipsis: true,
           fixed: 'left',
-          colKey: 'versionCount',
+          colKey: 'artifactVersionTotal',
         },
         {
-          title: '类型',
-          width: 120,
+          title: '状态',
+          width: 80,
           ellipsis: true,
           fixed: 'left',
-          colKey: 'type',
-        },
-        {
-          title: '是否安装',
-          width: 100,
-          ellipsis: true,
-          fixed: 'left',
-          colKey: 'isInstalled',
-        },
-        {
-          title: '仓库名称',
-          width: 100,
-          ellipsis: true,
-          colKey: "repoName"
+          colKey: 'status',
         },
         {
           title: '仓库地址',
@@ -161,16 +157,16 @@ export default Vue.extend({
           colKey: "repoUrl"
         },
         {
-          title: '生成时间',
-          width: 180,
+          title: '更新时间',
+          width: 160,
           ellipsis: true,
           fixed: 'left',
-          colKey: 'generated',
+          colKey: 'repoUpdateTime',
         },
         {
           align: 'left',
           fixed: 'right',
-          width: 150,
+          width: 180,
           colKey: 'op',
           title: '操作',
         },
@@ -194,7 +190,6 @@ export default Vue.extend({
         name: "",
         version: "",
         type: "",
-        namespace: "",
         pageNum: 1,
         pageSize: 10
       },
@@ -205,6 +200,12 @@ export default Vue.extend({
         type: "",
         action: "add",
         row: {}
+      },
+      confirm: {
+        header: "",
+        body: "",
+        operation: "update",
+        visible: false
       },
       typeList: []
     };
@@ -248,26 +249,48 @@ export default Vue.extend({
     // 点击安装
     handleClickInstall(row) {
       this.form.action = 'add'
-      this.form.header = '安装'+ row.name;
+      this.form.header = '安装' + row.name;
       this.form.row = row
     },
     // 执行安装
     handleInstall() {
       const row = this.form.row
-      this.$request.post("/helm/install?name="+row.name,{
+      this.$request.post("/helm/install?name=" + row.name, {
         params: {
           name: row.name,
           status: 'success'
         }
-      }).then(res=>{
+      }).then(res => {
         console.log("安装成功")
-        if (res.data.code ===200) {
+        if (res.data.code === 200) {
           this.$message.success(res.data.msg)
           this.form.visible = false;
         } else {
           this.$message.error(res.data.msg)
         }
       })
+    },
+    // 更新
+    handleClickUpdate(row) {
+      this.confirm.visible = true;
+      this.confirm.header = "确定要更新吗？";
+      this.confirm.body = `可能需要几十秒到几分钟时间`;
+      this.confirm.operation = 'update'
+      this.form = row;
+    },
+    // 对话框确定
+    handleClickConfirm() {
+      switch (this.confirm.operation) {
+        case "update":
+          this.$request.post("/helmRepo/update?repoName=" + this.form.repoName).then(res => {
+
+          })
+          break;
+        case "default":
+          break;
+      }
+      this.confirm.visible = false;
+      this.getList();
     },
     // 点击详情
     handleClickDetail() {
@@ -276,35 +299,6 @@ export default Vue.extend({
     },
     handleSetupContract() {
       this.$router.push('/prometheus/add');
-    },
-    // 点击卸载
-    handleClickDelete(row: { rowIndex: any,type: any }) {
-      this.deleteIdx = row.rowIndex;
-      this.deleteType = row.type;
-      this.confirmVisible = true;
-      console.log("this",this.deleteType)
-    },
-    onConfirmDelete() {
-      // 真实业务请发起请求
-      this.data.splice(this.deleteIdx, 1);
-      this.pagination.total = this.data.length;
-      const selectedIdx = this.selectedRowKeys.indexOf(this.deleteIdx);
-      if (selectedIdx > -1) {
-        this.selectedRowKeys.splice(selectedIdx, 1);
-      }
-      this.confirmVisible = false;
-      // 请求删除
-      this.$request.delete("/monitor/delete",{
-        params: {
-          index: this.deleteIdx,
-          type: this.deleteType
-        }
-      }).then(res=>{
-        this.$message.success(res.data.msg);
-      }).catch(err=>{
-
-      })
-      this.resetIdx();
     },
     onCancel() {
       this.resetIdx();
@@ -330,21 +324,22 @@ export default Vue.extend({
 
       })
     },
+    // 分页查询
     getList() {
       this.dataLoading = true;
-      this.$request.get('/helmRepo/page',{
-          params: this.searchForm
-        }).then((res) => {
+      this.$request.get('/helmRepo/page', {
+        params: this.searchForm
+      }).then((res) => {
         if (res.data.code === 200) {
           console.log(res.data)
           this.data = res.data.rows;
           this.pagination.total = res.data.total;
         }
       }).catch((e: Error) => {
-          console.log(e);
-        }).finally(() => {
-          this.dataLoading = false;
-        });
+        console.log(e);
+      }).finally(() => {
+        this.dataLoading = false;
+      });
     }
   },
 });
